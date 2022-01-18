ff03640b3683b0aeb270f72e79b0d86dfbf1232c
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-01-17 22:09:34 -0500

    002

--- MyAnswer/Section1/002.js
+++ MyAnswer/Section1/002.js
@@ -20,4 +20,36 @@ ex) (2)
 13 33 17
 - 출력예제 1
 NO
+
+삼각형을 만들기 위해서는 가장 긴 변을 제외한 나머지 2개의 변의 합이 가장 긴 변보다 길어야 한다
 */
+
+// max값을 먼저 찾는다.
+// 그 다음 두개를 더한값이 긴거 보다 작으면 return yes
+
+function solution(a, b, c) {
+  let max;
+  let two = [];
+  if (a < b) {
+    max = b;
+  } else {
+    max = a;
+  }
+
+  if (max < c) {
+    max = c;
+  }
+
+  if (max === a) {
+    two.push(b, c);
+  } else if (max === b) {
+    two.push(a, c);
+  } else {
+    two.push(a, b);
+  }
+
+  two = two.reduce((prev, next) => prev + next);
+  two > max ? console.log("yes") : console.log("no");
+}
+
+solution(1, 3, 2);
