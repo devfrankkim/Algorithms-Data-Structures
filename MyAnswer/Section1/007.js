8fb68a28eb3945820e7fb196ab614170c3ee2a32
heyfranksmile <heyfranksmile@gmail.com>
2022-04-17 21:43:57 -0400

    solution

--- MyAnswer/Section1/007.js
+++ MyAnswer/Section1/007.js
@@ -41,3 +41,17 @@ function stopCar(randomCar) {
 }
 
 stopCar(randomCar);
+
+// answer
+// No need to use array. Utilize variables.
+function solution(day, arr) {
+  let answer = 0;
+  for (let x of arr) {
+    if (x % 10 == day) answer++;
+  }
+
+  return answer;
+}
+
+arr = [25, 23, 11, 47, 53, 17, 33];
+console.log(solution(3, arr));
