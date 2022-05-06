28d0e2f5a956d43ff05de7d76e3d2c655142bcd8
heyfranksmile <heyfranksmile@gmail.com>
2022-05-06 22:50:33 +0900

    005

--- MyAnswer/Section1/005.js
+++ MyAnswer/Section1/005.js
@@ -53,12 +53,14 @@ console.log(solution1(5, 3, 7, 11, 2, 15, 17));
 function solutionAfter(arr) {
   let answer,
     min = Number.MAX_SAFE_INTEGER;
-  for (let i = 1; i < arr.length; i++) {
+  console.log(min);
+  for (let i = 0; i < arr.length; i++) {
+    console.log(min);
     if (arr[i] < min) min = arr[i];
   }
   answer = min;
   return answer;
 }
 
-let arr = [5, 7, 1, 3, 2, 9, 11];
+let arr = [5, 3, 7, 11, 2, 15, 17];
 console.log(solutionAfter(arr));
