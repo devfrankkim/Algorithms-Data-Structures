ae7c6a948fdfa3ac7c0bfe8a03ced79bd1d66942
heyfranksmile <heyfranksmile@gmail.com>
2022-09-23 17:02:37 +0900

    section 2 001

--- MyAnswer/Section2/003.js
+++ MyAnswer/Section2/003.js
@@ -49,3 +49,23 @@ function getNumbers(a, b) {
 }
 
 getNumbers(1, 2);
+
+function solution(a, b) {
+  let answer = "";
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] < 0 || b[i] < 0 || a[i] > 3 || b[i] > 3)
+      return "protect from other numbers";
+    else if (a[i] === b[i]) answer += "D ";
+    else if (a[i] === 1 && b[i] === 3) answer += "A ";
+    else if (a[i] === 2 && b[i] === 1) answer += "A ";
+    else if (a[i] === 3 && b[i] === 2) answer += "A ";
+    else answer += "B ";
+  }
+
+  return answer;
+}
+
+let a = [2, 3, 3, 4, 3];
+let b = [1, 1, 2, 2, 3];
+console.log(solution(a, b));
