761f4aea47054105777ea73144a815e30cd0a82f
heyfranksmile <heyfranksmile@gmail.com>
2022-10-06 20:33:09 +0900

    bruceforce

--- MyAnswer/Section4/005.js
+++ MyAnswer/Section4/005.js
@@ -2,11 +2,52 @@
 
     중요개념!
 
-      1~100부터 3개를 랜덤으로 뽑는다. 숫자가 중복 될 수도 있다.
-      3개 숫자 => sum
-      3장을 뽑을 수 있는 모든 경우를 기록합니다. 
-
+      sort ->  내림차순
+      앞에서부터 -> 3개씩 더하기. 
+      앞, 뒤 값 같으면 ++1 후에 해보기. 
+      3개 값 나눠서 for문 돌리고, 처음 3개 해보기.
 */
+
+/*
+    Pseudo code 
+        sort array 
+        i만큼 돌린다. 
+        let c1 = arr[i] + arr[i+1] + arr[i+2]
+        newArr = [c1, ]
+*/
+
+// [1,1,1,2,2,3,...., 99, 100]
+// 같은 번호가 있을 수 있음.
+// 기록한 값 중 K번째로 큰 수를 출력 하는 프로그램을 작성하세요.
+
+function solution(arr) {
+  arr.sort((a, b) => b - a);
+  const arrLength = arr.length;
+  let answer = 0;
+  // if(i === j) continue -> 다음 반복문
+
+  let newArr = [];
+
+  for (i = 0; i < arrLength; i++) {
+    let sum = arr[i] + arr[i + 1];
+    for (j = i + 2; j < arrLength; j++) {
+      // if(i === j) continue -> 다음 반복문
+      if (i === j) continue;
+
+      sum = sum + arr[j];
+      newArr.push(sum);
+    }
+    console.log(newArr);
+    newArr.sort((a, b) => b - a);
+    console.log(newArr);
+    // answer = Math.max(answer, newArr[2]);
+  }
+}
+
+var arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+
+solution(arr);
+
 /*
     현수는 1부터 100사이의 자연수가 적힌 N장의 카드를 가지고 있습니다. 
     같은 숫자의 카드가 여러장 있을 수 있습니다. 
@@ -22,9 +63,19 @@
     143
 */
 
-// [1,1,1,2,2,3,...., 99, 100]
-// 같은 번호가 있을 수 있음.
-// 기록한 값 중 K번째로 큰 수를 출력 하는 프로그램을 작성하세요.
+function solution1(n, k, card) {
+  let answer;
+  let tmp = new Set();
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let k = j + 1; k < n; k++) {
+        tmp.add(card[i] + card[j] + card[k]);
+      }
+    }
+  }
+  let a = Array.from(tmp).sort((a, b) => b - a);
+  answer = a[k - 1];
+  return answer;
+}
 
-function solution() {}
-solution();
+console.log(solution1(10, 3, arr));
