4ab179897addf8e600fb699cdf85f12c711644aa
heyfranksmile <heyfranksmile@gmail.com>
2023-01-17 21:54:18 +0900

    arr

---
+++ CT-AD/Section7/index1.js
@@ -0,0 +1,15 @@
+function chunk(array, size) {
+  const chunked = [];
+
+  for (let element of array) {
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+
+  return chunked;
+}
