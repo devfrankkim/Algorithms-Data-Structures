b86df490907bb38c0343ec46f95bfb71b28a1381
heyfranksmile <heyfranksmile@gmail.com>
2023-01-27 19:51:05 +0900

    capitalize first letter

--- CT-AD/Section9/index.js
+++ CT-AD/Section9/index.js
@@ -7,6 +7,36 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+let str = "yes man wow";
+
+function capitalize(str) {
+  return str
+    .split(" ")
+    .map((x) => x[0].toUpperCase() + x.slice(1))
+    .join(" ");
+}
+
+function test1(str) {
+  const words = [];
+  for (let word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return words.join(" ");
+}
+
+function test2(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+      console.log(result);
+    }
+  }
+
+  return result;
+}
 
 module.exports = capitalize;
