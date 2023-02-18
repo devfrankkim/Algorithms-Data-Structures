f0a95785a6c7842ef8307057fd21013e6c60cd37
heyfranksmile <heyfranksmile@gmail.com>
2023-02-18 16:50:09 +0900

    palindrome

---
+++ leetcode/easy/9.js
@@ -0,0 +1,27 @@
+// 9. Palindrome Number
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+
+var isPalindrome = function (x) {
+  return String(x).split("").reverse().join("") === String(x);
+};
+
+/*
+    Example 1:
+
+    Input: x = 121
+    Output: true
+    Explanation: 121 reads as 121 from left to right and from right to left.
+    Example 2:
+
+    Input: x = -121
+    Output: false
+    Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+    Example 3:
+
+    Input: x = 10
+    Output: false
+    Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+*/
